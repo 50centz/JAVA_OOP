939918779cc223963d1c5feacb209477e8f88425
Федоров Евгений <zen-g@mail.ru>
2023-03-09 15:28:14 +0300

    Edited task

--- HomeWork/HomeWork2/Main.java
+++ HomeWork/HomeWork2/Main.java
@@ -8,27 +8,6 @@ import static HomeWork.HomeWork2.Start.start;
 public class Main {
     public static void main(String[] args) {
       start();
-      // Zoo zoo = new Zoo();
-
-      // Cat cat = new Cat(1.1, 1.1, "gd", "fs", "fs", "fs", "fsd", "fs", "fs");
-      // Cat cat1 = new Cat(156.15, 156.14, "gd", "fs", "fs", "fs", "fsd", "fs", "fs");
-      // Wolf wolf = new Wolf(456.46, 7483.456, "fs", "fs", "fs", "fds");
-      // Wolf wolf1 = new Wolf(456.46, 7483.456, "fs", "fs", "fs", "fds");
-      // Wolf wolf2 = new Wolf(456.46, 7483.456, "fs", "fs", "fs", "fds");
-
-      // zoo.setZoo(cat);
-      // zoo.setZoo(cat1);
-      // zoo.setZoo(wolf);
-      // zoo.setZoo(wolf1);
-      // zoo.setZoo(wolf2);
-      // zoo.getAllAnimals();
-      
-      
-    
-     
-
-      
-      
             
     }
 }
