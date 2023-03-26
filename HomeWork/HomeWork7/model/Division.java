1db1549f8b0b1029c29f262c525ab48e5836f40d
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-03-26 15:15:37 +0300

    Sevent homework complete

--- HomeWork/HomeWork7/model/Division.java
+++ HomeWork/HomeWork7/model/Division.java
@@ -5,6 +5,9 @@ public class Division {
         String[] result = str.split("[/]");
         Double a = Double.parseDouble(result[0]);
         Double b = Double.parseDouble(result[1]);
+        if (b == 0){
+            return "0";
+        }
         Double res = a / b;
         String answer = Double.toString(res);
         return answer;  
--- HomeWork/HomeWork7/ui/DesktopUi.java
+++ HomeWork/HomeWork7/ui/DesktopUi.java
@@ -1,5 +1,10 @@
 package HomeWork.HomeWork7.ui;
 
+import javax.swing.JFrame;
+
 public class DesktopUi {
+
+    static JFrame frame;
+  
     
 }
